465be6efcf533588677271e818f90aa7135ab2b2
Manish <manishsuthar854@gmail.com>
2024-09-23 10:28:53 +0530

    c

--- redux/slices/apiSlice.js
+++ redux/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api", // optional
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://propelious-backend.onrender.com",
     prepareHeaders(headers) {
       headers.set("accept", "application/json ");
       let token = null;
